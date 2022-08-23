fd405765242beb3da3fe513ec760cc0fcbc89592
vmga09 <victor.gonzalez.a@gmail.com>
2022-08-23 16:34:20 -0400

    ultimop

--- 005appclima/models/busqueda.js
+++ 005appclima/models/busqueda.js
@@ -10,11 +10,19 @@ class Busqueda {
 
     async ciudad(lugar = ''){
         
+        
+       try {
         //Petición http
 
-       try {
-        const resp =  await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Santiago.json?limit=5&language=es&access_token=pk.eyJ1IjoiY3JpczdpYW5iYWNrIiwiYSI6ImNreWdndHJvcDBkdGYybm5zb216dWR6Y3AifQ.glT8w9hgQxLBh2laVu5JCg');
-        console.log(resp.data.features);
+        const instance = axios.create ({
+            baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+            params: {
+                'limit':5,
+                'language':'es',
+                'access_token':'pk.eyJ1IjoiY3JpczdpYW5iYWNrIiwiYSI6ImNreWdndHJvcDBkdGYybm5zb216dWR6Y3AifQ.glT8w9hgQxLBh2laVu5JCg'
+            }
+        });
+        const resp = await instance.get(); 
         return []; // retorna los lugares
        } catch (error) {
          
